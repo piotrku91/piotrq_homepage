5eb29277fe770bfd68858b467e69176aa6d07bda
piotrku <314159+Virtlink@@users.noreply.github.com>
2021-07-23 00:40:51 +0200

    button objects

--- conf/classes.php
+++ conf/classes.php
@@ -2,40 +2,108 @@
 
 class SQLLoader
 {
-function newQuery(& $connection, $query)
-{
-    $result= $connection->query($query);
-    return $result;
 
+    function newQuery(&$connection, $query)
+    {
+        $result = $connection->query($query);
+        return $result;
+    }
+
+    function isAnyRow(&$results)
+    {
+        if ($results->num_rows > 0) {
+            return true;
+        };
+        return false;
+    }
 }
 
-}
-
-class LinkLoader
+class LinkLoader extends SQLLoader
 {
-function loadCategories(& $connection, $table="links")
-{
-  $sql = "SELECT category from " .$table;
-  $result= $connection->query($sql);
 
-  return $result;
+    function loadCategories(&$connection, $table = "links")
+    {
+        return $this->newQuery($connection, "SELECT category from " . $table);
+    }
 
-}
+    function loadSection(&$connection, $section, $table = "links")
+    {
+        $result = $this->newQuery($connection, "SELECT * from " . $table . " WHERE category=\"" . $section . "\"");
+        echo "<div style=\"background-color: #1f8759\">" . $section . "</div>";
 
-function loadSection(& $connection, $section, $table="links")
-{
-  $sql = "SELECT * from " .$table." WHERE category=\"".$section."\"";
-  echo "<div style=\"background-color: #1f8759\">".$section."</div>";
-  $result = $connection->query($sql);
-
-            if ($result->num_rows > 0) {
-                foreach ($result as $row) {
-                  return "<div>".$row["url"]." - ".$row["name"]." - ".$row["desc"]."</div>";
-                }
+        if ($this->isAnyRow($result)) {
+            foreach ($result as $row) {
+                return "<div>" . $row["url"] . " - " . $row["name"] . " - " . $row["desc"] . "</div>";
             }
-             else return "Brak linków.";
+        } else return "Brak linków.";
+    }
+}
+
+
+class MainVarsLoader extends SQLLoader
+{
+    function getVar(&$connection, $stringkey, $error_msg)
+    {
+
+        $result = $this->newQuery($connection, "SELECT value from page_string_table WHERE stringkey=\"" . $stringkey . "\";");
+        if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {
+            return $row[0];
+        } else return $error_msg;
+    }
+}
+
+
+class Button 
+{
+    protected $ref;
+    protected $operation;
+    protected $caption;
+
+    public function __construct($ref, $operation, $caption)
+    {
+        $this->ref = $ref;
+        $this->operation = $operation;
+        $this->caption = $caption;
+    }
+
 
 }
-};
 
-?>
+class OperationButton extends Button
+{
+    
+    function getCode()
+    {
+        $code = "<form action=\"index.php?ref=" . $this->ref . "\" role = \"form\" method = \"post\">";
+        $code .= "<input type=\"submit\" name=\"" . $this->operation . "\" value=\"" . $this->caption . "\">";
+        $code .= "</form><br>";
+        return $code;
+    }
+
+    function getCodeOnlyButton()
+    {
+        $code = "<input type=\"submit\" name=\"" . $this->operation . "\" value=\"" . $this->caption . "\">";
+        return $code;
+    }
+}
+
+class UrlExButton extends Button
+{
+
+    public function __construct($ref, $operation, $caption, $url_ex)
+    {
+        $this->ref = $ref;
+        $this->operation = $operation;
+        $this->caption = $caption;
+        $this->url_ex = $url_ex;
+    }
+    
+    function getCode()
+    {
+        
+        $code = "<form action=\"index.php?ref=" . $this->ref ."&". $this->url_ex . "\" role = \"form\" method = \"post\">";
+        $code .= "<input type=\"submit\" name=\"" . $this->operation . "\" value=\"" . $this->caption . "\">";
+        $code .= "</form><br>";
+        return $code;
+    }
+}
--- conf/loader.php
+++ conf/loader.php
@@ -8,27 +8,19 @@ include "classes.php"; // include helpful classes
 $ref = $_GET['ref'];
 $force = $_GET['force'];
 
+// Create objects
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+$queryGate = new SQLLoader();
+$pageVars = new MainVarsLoader();
+
 
 // Get some global variables
-
-$pages_folder = "fresh/";
-$error_msg="MySQL Query error";
-
-$result = $conn->query("SELECT value from page_string_table WHERE stringkey=\"mainpage_title\";");
-if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {$mainpage_title = $row[0];} else $mainpage_title=$error_msg;
-
-$result = $conn->query("SELECT value from page_string_table WHERE stringkey=\"footer_text\";");
-if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {$footer_text = $row[0];} else $footer_text=$error_msg;
-
-$result = $conn->query("SELECT value from page_string_table WHERE stringkey=\"yt_link\";");
-if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {$yt_link = $row[0];} else $yt_link=$error_msg;
-
-$result = $conn->query("SELECT value from page_string_table WHERE stringkey=\"gh_link\";");
-if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {$gh_link = $row[0];} else $gh_link=$error_msg;
-
-$result = $conn->query("SELECT value from page_string_table WHERE stringkey=\"li_link\";");
-if (($row = mysqli_fetch_row($result)) && mysqli_num_rows($result)) {$li_link = $row[0];} else $li_link=$error_msg;
+$mainpage_title = $pageVars->getVar($conn, "mainpage_title", "Strona domowa");
+$footer_text = $pageVars->getVar($conn, "footer_text", "Projekt strony w HTML, CSS oraz programowanie w PHP, MySQL wykonane przez PiotrQ");
+$yt_link = $pageVars->getVar($conn, "yt_link", "Strona domowa");
+$gh_link = $pageVars->getVar($conn, "gh_link", "Strona domowa");
+$li_link = $pageVars->getVar($conn, "li_link", "Strona domowa");
+$pages_folder = $pageVars->getVar($conn, "pages_folder", "fresh/");
 
             
 if ($ref=="" || $ref=="start") { $actual_page = $pages_folder . "start"; } 
